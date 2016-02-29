1b79462596bef486a8e5962a95c037e5b964f9d1
Rodolfo <rodolfodeangeli@gmail.com>
2016-02-29 19:41:00 -0300

    first commit

---
+++ conFusion/www/js/services.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('conFusion.services', ['ngResource'])
+  .constant("baseURL", "http://localhost:3000/")
+  .service('menuFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+    this.getDishes = function() {
+      return $resource(baseURL + "dishes/:id", null, { 'update': { method: 'PUT' } });
+    };
+    // implement a function named getPromotion
+    // that returns a selected promotion.
+    this.getPromotion = function() {
+      return $resource(baseURL + "promotions/:id");;
+    }
+  }])
+  .factory('corporateFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+    return $resource(baseURL + "leadership/:id");
+  }])
+  .factory('feedbackFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+    return $resource(baseURL + "feedback/:id");
+  }])
+  .service('contactusService', ['$resource', 'baseURL', function($resource, baseURL) {
+    this.getAddress = function() {
+      return $resource(baseURL + "address");
+    }
+  }])
+  .service('historyService', ['$resource', 'baseURL', function($resource, baseURL) {
+    this.getHistory = function() {
+      return $resource(baseURL + "history");
+    }
+  }]);
